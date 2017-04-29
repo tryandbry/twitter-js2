e4c83fda3a8f9e0303ea3e0b5e3b7fac980e4d54
bryan <bryan@localhost.localdomain>
2017-04-28 23:43:21 -0500

    nunjucks render test is working

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
+// modules
 const express = require('express');
 const app = express();
+const nunjucks = require('nunjucks');
 
+//configure modules
+nunjucks.configure('views',{noCache: true});
+app.engine('html',nunjucks.render);
+app.set('view engine','html');
+
+// middleware
+app.use(function(request,response,next){
+  console.log(request.method + " " +
+              request.path + " ");
+  
+  next(); 
+});
+
+
+// routes
 app.get('/',function(request,response){
-  console.log("Welcome!\n");
-
-  response.send("Welcome!");
+  nunjucks.render('index.html',{ title:'An Example',
+                                people:[ { name: 'Gandalf' },
+                                         { name: 'Frodo' },
+                                         { name: 'Hermione' } ]
+                               },function(error, output){
+  console.log(output);
+  response.send(output);
+});
 });
 
 app.listen(3000);
